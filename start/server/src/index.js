31871306459b46dbf6f565dd52a5c9964e833026
Rafa Garés <rafael.gares@xing.com>
2019-05-22 10:20:54 +0200

    Hook up your data sources

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,9 +1,19 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const { createStore } = require('./utils');
 
-const server = new ApolloServer({ 
-    typeDefs,
-    playground: { version: '1.7.25' } 
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  }),
+  playground: { version: '1.7.25' } 
 });
 
 server.listen().then(({ url }) => {
