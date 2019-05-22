d126f0f27079460751f5c6c7862fb0bfb8c04f60
Rafa Garés <rafael.gares@xing.com>
2019-05-22 12:03:39 +0200

    Write your graph's resolvers

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -3,7 +3,16 @@ const { gql } = require('apollo-server');
 const typeDefs = gql`
 
     type Query {
-        launches: [Launch]!
+        launches( # replace the current launches query with this one.
+            """
+            The number of results to show. Must be >= 1. Default = 20
+            """
+            pageSize: Int
+            """
+            If you add a cursor here, it will only return results _after_ this cursor
+            """
+            after: String
+        ): LaunchConnection!
         launch(id: ID!): Launch
         # Queries for the current user
         me: User
@@ -53,6 +62,17 @@ const typeDefs = gql`
         success: Boolean!
         message: String
         launches: [Launch]
+    }
+
+    """
+    Simple wrapper around our list of launches that contains a cursor to the
+    last item in the list. Pass this cursor to the launches query to fetch results
+    after these.
+    """
+    type LaunchConnection {
+        cursor: String!
+        hasMore: Boolean!
+        launches: [Launch]!
     }
 
 `;
